2f9c5026e729ac72e9c4b3d0c0062835c4c8609f
Diogo <diomoura@BRREDIOMOURA.atrame.deloitte.com>
2026-03-18 22:40:07 -0300

    json conversion script from raw to seed

---
+++ scipts/json_to_csv.py
@@ -0,0 +1,47 @@
+import os
+import json
+import csv
+from pathlib import Path
+
+RAW_DIR = Path("../data_raw")              # Pasta com JSONs
+SEEDS_DIR = Path("../lh_nautical/seeds")   # Pasta seeds do dbt
+
+def convert_json_to_csv(input_file: Path, output_file: Path):
+    """Converte um arquivo JSON em CSV."""
+    with open(input_file, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    # Se o JSON for um único objeto, transforma em lista
+    if isinstance(data, dict):
+        data = [data]
+
+    # Descobre os campos automaticamente
+    keys = sorted(list({key for row in data for key in row.keys()}))
+
+    with open(output_file, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(data)
+
+    print(f"✔ Convertido: {input_file.name} → {output_file.name}")
+
+
+def main():
+    #caso pasta seed nao exista, criar
+    SEEDS_DIR.mkdir(parents=True, exist_ok=True)
+
+    for file in RAW_DIR.glob("*.json"):
+        output_file = SEEDS_DIR / f"{file.stem}.csv"
+        convert_json_to_csv(file, output_file)
+    for file in RAW_DIR.glob("*.csv"):
+        output_file = SEEDS_DIR / file.name
+        os.replace(file, output_file)
+        print(f"✔ Movido: arquivos csv {file.name} → {output_file.name}")
+
+
+    print("\n🎉 Conversão e atualizacção de dados concluída! Agora rode:")
+    print("   dbt seed\n")
+
+
+if __name__ == "__main__":
+    main()
